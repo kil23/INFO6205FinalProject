eda0d592d7ecc44ea9514cd4fc9466973265d389
tanaysaxena97 <tanaysaxena97@gmail.com>
2021-11-23 19:42:45 -0500

    added tests and refactored code

--- src/main/java/edu/neu/coe/info6205/MSDRadixSort.java
+++ src/main/java/edu/neu/coe/info6205/MSDRadixSort.java
@@ -1,55 +1,41 @@
 package edu.neu.coe.info6205;
 
 import edu.neu.coe.info6205.utils.FileUtil;
+import edu.neu.coe.info6205.utils.SortUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 // n: input size, d: number of distinct elements, complexity - O(n + d*lg(d))
 public class MSDRadixSort {
-    private final Character paddingChar = ' ';
     private Integer maxStringLength;
     private List<String> aux;
+    private SortUtils sortUtils;
     public List<String> sort(List<String> xs) {
         if (xs.size() == 0) return xs;
         aux = new ArrayList<>();
-        maxStringLength = xs.get(0).length();
-        for (int i = 0; i < xs.size(); i++) {
-            aux.add("");
-            maxStringLength = Math.max(maxStringLength, xs.get(i).length());
-        }
-        xs = xs.stream().map(currStr -> {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < maxStringLength - currStr.length(); i++) sb.append(paddingChar);
-            return currStr + sb.toString();
-        }).collect(Collectors.toList());
+        sortUtils = new SortUtils();
+        maxStringLength = sortUtils.getMaxStringLength(xs);
+        for (int i = 0; i < xs.size(); i++) aux.add("");
+        xs = sortUtils.padMSDStrings(xs);
         xs = sort(xs, 0, xs.size() - 1, 0);
-        return xs.stream().map(curStr -> curStr.trim()).collect(Collectors.toList());
+        return sortUtils.removeMSDPadding(xs);
     }
 
     private List<String> sort(List<String> xs, int l, int r, int d) {
         if (d >= maxStringLength) return xs;
-        Map<Character, Integer> count = new TreeMap<>();
         List<Integer> indices = new ArrayList<>();
         // counting step
-        for (int i = l; i <= r; i++) {
-            Character c = xs.get(i).charAt(d);
-            if (count.containsKey(c)) count.put(c, count.get(c) + 1);
-            else count.put(c, 1);
-        }
+        Map<Character, Integer> count = sortUtils.getFrequencyTreeMap(xs, l, r, d);
+
         // TODO: additionally we can use an LinkedHashMap to store the sorted and counted frequencies from the map, and iterate over the in the future steps. (maybe)
         // TODO: use msd radix sort to sort buckets of strings having same lengths, and then use k-way merge of sorted lists https://leetcode.com/problems/merge-k-sorted-lists/
         // populate indices array for iteration
         for (int i: count.values()) indices.add(i);
-//        count.entrySet().stream().forEach(System.out::println);
+
         // cumulative sum step  ---  for i in range(1, n): a[i] += a[i-1]
-        Integer sm = 0;
-        for (Map.Entry<Character, Integer> entry: count.entrySet()) {
-            entry.setValue(sm + entry.getValue());
-            sm = entry.getValue();
-        }
+        count = sortUtils.cumulativeSum(count);
+
         // populate the aux array from l to r
         for (int i = l; i <= r; i++) {
             Character c = xs.get(i).charAt(d);
@@ -57,10 +43,12 @@ public class MSDRadixSort {
             aux.set(newIdx, xs.get(i));
             count.put(c, count.get(c) - 1);
         }
+
         // copy aux to xs
         for (int i = l; i <= r; i++) {
             xs.set(i, aux.get(i));
         }
+
         // recursive step
         int lo = l;
         for (int i = 0; i < indices.size(); i++) {
